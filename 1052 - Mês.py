b7db67cbc30349dc97a083f4c7cadacfb028855e
Guilherme PC <guilhermeperes0202@gmail.com>
2024-12-11 15:25:05 -0300

    Resolução 1052

--- 1052 - Mês.py
+++
@@ -1,6 +0,0 @@
-pesquisa = int(input(""))
-
-mes = ["Ajuste","January", "February", "March","April","May","June","July","August","September","October","November","December"]
-
-if 0 <= pesquisa < len(mes):
-    print(mes[pesquisa])
